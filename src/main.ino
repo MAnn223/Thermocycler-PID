51ff5851603da7710291cb90eaa79fb59c249f8d
Meena <annmm537@gmail.com>
2024-05-06 12:09:16 -0700

    tempCurvePID

--- src/main.ino
+++ src/main.ino
@@ -13,40 +13,151 @@ int Kd = 0.02;
 double tempError;
 double targetTemp;
 double currentTemp;
+const int HEAT = 5;
+const int COOL = 6;
+const int SDA = 23;
+const int SCL = 24;
+const int READ_TEMP = 23;
+const int TEMP_CLK = 24;
+int heatState = 0;
+int coolState = 0;
+const int INITIALSTATE = 0; // 0 for HEAT, 1 for COOL
+unsigned long phase12 = 195000; //3 min and 15 sec, 95
+unsigned long phase3 = 30000; //30 sec, 60
+unsigned long phase4 = 15000; //15 sec, 72
+unsigned long phase6 = 240000; //4 min
+unsigned long oneCycle = 240000;
+int cycleCount = 0;
+
 PID myPID(&difference, &driverOut, &setPoint,Kp,Ki,Kd, DIRECT);
 void setup() {
   // put your setup code here, to run once:
   myPID.SetMode(AUTOMATIC);
-  //attachInterrupt(23, control, CHANGE);
+  attachInterrupt(23, calculate, CHANGE);
   //8 pwm, 7 tacho, 27 5v
-  pinMode(5, OUTPUT); //Heat
-  pinMode(6, OUTPUT); //Cool
-  pinMode(23, OUTPUT); //SDA
-  pinMode(24, INPUT); //SCL
-  interrupts();
-  noInterrupts();
+  pinMode(HEAT, OUTPUT); //Heat
+  pinMode(COOL, OUTPUT); //Cool
+  pinMode(SDA, OUTPUT); //SDA
+  pinMode(SCL, INPUT); //SCL
+  heatState = INITIALSTATE;
+  coolState = !INITIALSTATE;
+  Serial.begin(9600);
 
 }
-
+void loop() {
+  tempCurve();
+  //figure out what sampling rate would be best
+  delay(1000);  //delay for 1 sec
+}
 void control() {
   //noInterrupts();
   //get temp data
   //interrupts();
   
-  myPID.Compute();
-  digitalWrite(5, driverOut);
 };
+
+double getTemp() {
+  //code to get temp reading 
+}
+void updateTemp() {
+  if (driverOut > 0) {
+    // Turn on heating
+    digitalWrite(HEAT, driverOut);
+    digitalWrite(COOL, 0);
+  } else if (driverOut < 0) {
+    // Turn on cooling
+    digitalWrite(HEAT, 0);
+    digitalWrite(COOL, driverOut);
+  } else {
+    // Turn off 
+    digitalWrite(HEAT, 0);
+    digitalWrite(COOL, 0);
+  }
+}
 
 void calculate() {
   tempError = currentTemp - targetTemp;
   currentTime = millis();
   difference = currentTime - prevTime;
   prevTime = currentTime;
+  currentTemp = getTemp();
+  myPID.Compute();
+  updateTemp();
 }
-//attachInterrupt();
-// void loop() {
-//   // put your main code here, to run repeatedly:
-  
 
-// }
+//functions from simulation code
+void swapModes(int &heatState, int &coolState);
+void onHeat();
+void onCool();
+void heatUp(int targetTemp);
+void coolDown(int targetTemp);
+void shutDown();
+
+
+void tempCurve() 
+{
+  
+  for(int i = 0; i <30; i++) {
+    unsigned long currentTime = millis();
+    while (currentTemp < oneCycle) {
+      if(currentTime < phase12) {
+        targetTemp = 95;
+      }
+      else if (currentTime < phase12 + phase3) {
+        targetTemp = 60;
+      }
+      else if (currentTime < phase12 + phase3 + phase4) {
+        targetTemp = 72;
+      } else {
+        break;
+      }
+    }
+    cycleCount++;
+  }
+
+}
+
+
+void swapModes(int &heatState, int &coolState)
+{
+  heatState = coolState;
+  coolState = !heatState;
+  digitalWrite(HEAT, heatState);
+  digitalWrite(COOL, coolState);
+}
+
+void onHeat() {
+  digitalWrite(COOL, LOW);
+  digitalWrite(HEAT, HIGH);
+}
+
+void onCool() {
+  digitalWrite(HEAT, LOW);
+  digitalWrite(COOL, HIGH);
+}
+
+void heatUp(int targetTemp) {
+  while (currentTemp < targetTemp) {
+    onHeat();
+    delay(100);
+    currentTemp++;
+    Serial.print(currentTemp);
+  }
+  shutDown();
+}
+
+void coolDown(int targetTemp) {
+   while (currentTemp > targetTemp) {
+    onCool();
+    delay(100);
+    currentTemp--;
+    Serial.print(currentTemp);
+  }
+  shutDown();
+}
+
+void shutDown() {
+  digitalWrite(HEAT, LOW);
+  digitalWrite(COOL, LOW);
+}
 
