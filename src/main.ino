1be79abf510bece35cc7e9e8ce3b2212f6de7df0
Meena <annmm537@gmail.com>
2024-05-06 11:17:57 -0700

    test

--- src/main.ino
+++ src/main.ino
@@ -1,18 +1,49 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include <PID_v1.h>
+#include <PID_v1.h>
+double driverOut = 10;
+double difference = 10;
+double setPoint = 10;
+unsigned long changeTime = 0;
+unsigned long currentTime = millis();
+unsigned long prevTime = millis();
+int Kp = 0.01;
+int Ki = 0.2;
+int Kd = 0.02;
+double tempError;
+double targetTemp;
+double currentTemp;
+PID myPID(&difference, &driverOut, &setPoint,Kp,Ki,Kd, DIRECT);
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  myPID.SetMode(AUTOMATIC);
+  //attachInterrupt(23, control, CHANGE);
+  //8 pwm, 7 tacho, 27 5v
+  pinMode(5, OUTPUT); //Heat
+  pinMode(6, OUTPUT); //Cool
+  pinMode(23, OUTPUT); //SDA
+  pinMode(24, INPUT); //SCL
+  interrupts();
+  noInterrupts();
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void control() {
+  //noInterrupts();
+  //get temp data
+  interrupts();
+  tempError = currentTemp - targetTemp;
+  currentTime = millis();
+  difference = currentTime - prevTime;
+  prevTime = currentTime;
+  myPID.Compute();
+  digitalWrite(5, driverOut);
+};
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+//attachInterrupt();
+// void loop() {
+//   // put your main code here, to run repeatedly:
+  
+
+// }
+
